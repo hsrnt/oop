a7c3047c03c5fdf51d2d66f0e71d34540f1fed01
Yuma Soerianto <yuuummmaaa@gmail.com>
2016-03-14 12:34:37 +1100

    did Charecter.swift

---
+++ UdemyChallenge08-OOP/Charecter.swift
@@ -0,0 +1,23 @@
+//
+//  Charecter.swift
+//  UdemyChallenge08-OOP
+//
+//  Created by Yuma Soerianto on 14/03/2016.
+//  Copyright © 2016 Yuma Soerianto. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    var name = "Bob"
+    var hp = 100
+    var died: Bool {
+        if hp <= 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func attack() {}
+}
