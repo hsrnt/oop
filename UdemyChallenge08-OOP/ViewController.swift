83b2f92e3263afbadd2f9fa9c62386aa0a8bde53
Yuma Soerianto <yuuummmaaa@gmail.com>
2016-03-14 11:48:34 +1100

    initial commit

---
+++ UdemyChallenge08-OOP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UdemyChallenge08-OOP
+//
+//  Created by Yuma Soerianto on 14/03/2016.
+//  Copyright © 2016 Yuma Soerianto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ 
+
+}
+
